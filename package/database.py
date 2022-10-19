c3a50ba0bd379aaafb792d59c03b1c7b065adacb
melakhvision <45154979+melakhvision@users.noreply.github.com>
2022-10-19 11:42:47 +0000

    Update database.py

--- package/database.py
+++ package/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import pymsql
+import pymysql
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # db=_mysql.connect("207.154.236.252","root","winkerdev630",)
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:winkerdev630@207.154.236.252/devdb"
